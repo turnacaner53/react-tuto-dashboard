54767eabefb6acea5f1c6cf11415724ce8f5ab82
Caner Turna <turnacaner53@outlook.com>
2024-05-09 10:18:08 +0300

    refactor: Update useSearchStore in foot-recipe to persist favourites in local storage

--- src/features/foot-recipe/store.js
+++ src/features/foot-recipe/store.js
@@ -1,15 +1,29 @@
 import { create } from 'zustand';
 
+
+
 const useSearchStore = create((set) => ({
   search: '',
   setSearch: (search) => set({ search }),
   loading: false,
   setLoading: (loading) => set({ loading }),
   list: [],
-  setList: (list) => set(()=>({list})),
-  favourites: [],
-  addFavourite: (recipe) => set((state) => ({ favourites: [...state.favourites, recipe] })),
-  removeFavourite: (id) => set((state) => ({ favourites: state.favourites.filter((item) => item.id !== id) })),
+  setList: (list) => set({ list }),
+  favourites: JSON.parse(localStorage.getItem('foot-favourites')) || [],
+  addFavourite: (recipe) => {
+    set((state) => {
+      const updatedFavourites = [...state.favourites, recipe];
+      localStorage.setItem('foot-favourites', JSON.stringify(updatedFavourites));
+      return { favourites: updatedFavourites };
+    });
+  },
+  removeFavourite: (id) => {
+    set((state) => {
+      const updatedFavourites = state.favourites.filter((item) => item.id !== id);
+      localStorage.setItem('foot-favourites', JSON.stringify(updatedFavourites));
+      return { favourites: updatedFavourites };
+    });
+  },
 }));
 
 export default useSearchStore;
